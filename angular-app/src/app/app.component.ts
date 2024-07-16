7083e6327b24841b4d4f8a76b3e5ce4638453860
Murat Koyun <muratk1845@gmail.com>
2024-07-16 22:17:49 +0200

    feat: Rxjs best practises

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+  title(title: any) {
+    throw new Error('Method not implemented.');
+  }
+ constructor () {
+
+  const promise1 = new Promise((resolve) => {
+setTimeout(() => {
+  resolve('Resolved!')
+}, 4000)
+  })
+
+  const message$ = from(promise1)
+
+  message$.subscribe((message) => {
+    console.log('message:', message)
+  })
+
+  const numbers$ = of([1,2,3,4,5])
+
+  numbers$.subscribe((data) => {
+    console.log(data, '...')
+  })
+
+ }
+  
 }
